f728626ab096909585117043266de41f5092fc42
Clyde Mendonca <clyde.mendonca01@gmail.com>
2019-08-10 12:52:41 +0530

    Service and Repository annoations adde

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -3,9 +3,11 @@ package com.pluralsight.service;
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
 import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
     private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
