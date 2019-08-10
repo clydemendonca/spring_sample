fcc33ac1a2be2d2962cbb968b5b9c2dc866b014f
Clyde Mendonca <clyde.mendonca01@gmail.com>
2019-08-10 12:32:24 +0530

    Autowire byName added

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -9,6 +9,9 @@ public class CustomerServiceImpl implements CustomerService {
 
     private CustomerRepository customerRepository;
 
+    public CustomerServiceImpl() {
+    }
+
     public CustomerServiceImpl(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
