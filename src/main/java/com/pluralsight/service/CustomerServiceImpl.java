88c9736fb5291d2dcff5e1448b01e33747eef912
Clyde Mendonca <clyde.mendonca01@gmail.com>
2019-08-10 12:18:45 +0530

    Constructor Injection

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -1,20 +1,24 @@
 package com.pluralsight.service;
 
 import com.pluralsight.model.Customer;
-import com.pluralsight.repository.CustomerRespository;
+import com.pluralsight.repository.CustomerRepository;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRespository customerRespository;
+    private CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     @Override
     public List<Customer> findAll() {
-        return customerRespository.findAll();
+        return customerRepository.findAll();
     }
 
-    public void setCustomerRespository(CustomerRespository customerRespository) {
-        this.customerRespository = customerRespository;
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
     }
 }
