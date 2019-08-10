19f17e700fa82734e0e8e24b1997f346ad4e5e64
Clyde Mendonca <clyde.mendonca01@gmail.com>
2019-08-10 14:10:06 +0530

    Setter Injection

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -1,12 +1,16 @@
 package com.pluralsight;
 
+import com.pluralsight.model.Customer;
 import com.pluralsight.service.CustomerService;
 import com.pluralsight.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
-        CustomerService customerService = new CustomerServiceImpl();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        CustomerService customerService = applicationContext.getBean("customerService", CustomerService.class);
         System.out.println(customerService.findAll().get(0).getFirstName());
     }
 
