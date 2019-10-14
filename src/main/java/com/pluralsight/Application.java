e6e493959bd21647e857b8553123c33cca8a69b7
Clyde Mendonca <clyde.mendonca01@gmail.com>
2019-10-14 15:32:34 +0530

    Singleton scope added

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -10,7 +10,15 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         CustomerService service = applicationContext.getBean("customerService", CustomerService.class);
+
+        System.out.println(service);
+
+        CustomerService service2 = applicationContext.getBean("customerService", CustomerService.class);
+
+        System.out.println(service2);
+
         System.out.println(service.findAll().get(0).getFirstName());
+
     }
 
 }
