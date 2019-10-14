e131aa5ff9599ece757b104272597f6aa8b28191
Clyde Mendonca <clyde.mendonca01@gmail.com>
2019-10-14 15:24:02 +0530

    Autowired added

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,11 +2,15 @@ package com.pluralsight.service;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
+//    @Autowired
     private CustomerRepository customerRepository;
 
     public CustomerServiceImpl() {
@@ -22,6 +26,7 @@ public class CustomerServiceImpl implements CustomerService {
         return customerRepository.findAll();
     }
 
+    @Autowired
     public void setCustomerRepository(CustomerRepository customerRepository) {
         System.out.println("We are using setter injection");
         this.customerRepository = customerRepository;
