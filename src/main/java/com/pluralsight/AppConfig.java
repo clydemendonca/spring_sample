55db2fdc7d376d4e8e6e9edb379b7d4a747bb1a4
Clyde Mendonca <clyde.mendonca01@gmail.com>
2019-10-14 16:09:45 +0530

    App Properties

--- src/main/java/com/pluralsight/AppConfig.java
+++ src/main/java/com/pluralsight/AppConfig.java
@@ -4,12 +4,16 @@ import com.pluralsight.repository.CustomerRepository;
 import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 import com.pluralsight.service.CustomerService;
 import com.pluralsight.service.CustomerServiceImpl;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan({"com.pluralsight"})
+@PropertySource("app.properties")
 public class AppConfig {
 
 //    @Bean(name = "customerService")
@@ -24,6 +28,11 @@ public class AppConfig {
 //    public CustomerRepository getCustomerRepository() {
 //        return new HibernateCustomerRepositoryImpl();
 //    }
+
+    @Bean
+    PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
 
 }
--- src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,8 +10,14 @@ import java.util.List;
 @Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+    @Value("${dbUsername}")
+    private String dbUsername;
+
     @Override
     public List<Customer> findAll() {
+
+        System.out.println(dbUsername);
+
         List<Customer> customerList = new ArrayList<>();
 
         Customer customer = new Customer();
